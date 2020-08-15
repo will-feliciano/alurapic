510ed0e7e299a6cd6cb246fe70e51432b810244a
William <william.lopes@pravaler.com.br>
2020-08-15 17:05:30 -0300

    [master] Commit de Momento de todo projeto

---
+++ src/app/home/signin/signin.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { Router } from '@angular/router';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
+
+@Component({
+    templateUrl: './signin.component.html'
+})
+export class SignInComponent implements OnInit {
+
+    loginForm: FormGroup;
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private router: Router,
+        private platformDetectionService: PlatformDetectorService) { }
+
+    ngOnInit(): void {
+        this.loginForm = this.formBuilder.group({
+            userName: ['', Validators.required],
+            password: ['', Validators.required]
+        });       
+    }
+
+    login() {
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+
+        this.authService
+            .authenticate(userName, password)
+            .subscribe(
+                () => this.router.navigate(['user', userName]),
+                err => {
+                    console.log(err);
+                    this.loginForm.reset();
+                    this.platformDetectionService.isPlatformBrowser() &&
+                        this.userNameInput.nativeElement.focus();
+                    alert('Usuário incorreto.');
+                }
+            );
+        
+    }
+}
