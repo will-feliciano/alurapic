2ccdf7819e8afc82284986b84b8eb224771d87c8
William <william.lopes@pravaler.com.br>
2020-09-08 00:05:21 -0300

    Finalizado a implementação dos alerts e not-found

--- src/app/photos/photo-detail/photo-details.component.ts
+++ src/app/photos/photo-detail/photo-details.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { Photo } from "../photo/photo";
 import { PhotoService } from "../photo/photo.service";
 import { Observable } from "rxjs";
+import { AlertService } from "src/app/shared/components/alert/alert.service";
+import { UserService } from "src/app/core/user/user.service";
 
 @Component({
     templateUrl: './photo-details.component.html',
@@ -17,7 +19,9 @@ export class PhotoDetailsComponent implements OnInit {
     constructor(
         private route: ActivatedRoute,
         private photoService: PhotoService,
-        private router: Router
+        private router: Router,
+        private alertService: AlertService,
+        private userService: UserService
     ) {}
 
     ngOnInit(): void {
@@ -27,12 +31,24 @@ export class PhotoDetailsComponent implements OnInit {
         //     .findById(id)
         //     .subscribe(photo => this.photo = photo);
 
-        this.photo$ = this.photoService.findById(this.photoId);        
+        this.photo$ = this.photoService.findById(this.photoId);
+        this.photo$.subscribe(() => {}, err => {
+            console.log(err);
+            this.router.navigate(['not-found']);
+        });   
     }
 
     remove() {
         this.photoService
             .removePhoto(this.photoId)
-            .subscribe(() => this.router.navigate(['']));
+            .subscribe(
+                () => {
+                    this.alertService.success("Photo removed", true);
+                    this.router.navigate(['/user', this.userService.getUserName()]);
+                },
+                err => {
+                    console.log(err);
+                    this.alertService.warning('Could not delete the photo!', true);
+                });
     }
 }
