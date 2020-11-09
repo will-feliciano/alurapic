0e3081c39cddd93be654057611c70eeb9987af6c
William <william.lopes@pravaler.com.br>
2020-11-09 16:00:35 -0300

    setar o title manualmente

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/auth/auth.service';
 import { Router } from '@angular/router';
 import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -16,9 +17,12 @@ export class SignInComponent implements OnInit {
         private formBuilder: FormBuilder,
         private authService: AuthService,
         private router: Router,
-        private platformDetectionService: PlatformDetectorService) { }
+        private platformDetectionService: PlatformDetectorService,
+        private titleService: Title
+    ) { }
 
     ngOnInit(): void {
+        this.titleService.setTitle('Login');
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
