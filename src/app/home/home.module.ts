dfe74850e1977fab956c762b25f9a0d84060f7b3
William <william.lopes@pravaler.com.br>
2020-11-11 00:01:11 -0300

    finalizando progresso

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,6 @@ import { SignInComponent } from './signin/signin.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
-//import { RouterModule } from '@angular/router';
 import { SignUpComponent } from './signup/signup.component';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing.module';
--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -4,6 +4,8 @@ import { PhotoService } from '../photo/photo.service';
 import { Router } from '@angular/router';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { UserService } from 'src/app/core/user/user.service';
+import { HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'ap-photo-form',
@@ -15,6 +17,7 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
   preview: string;
+  percentDone: number = 0;
 
   constructor( 
     private formBuilder: FormBuilder,
@@ -37,10 +40,22 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments').value;
     this.photoService
       .upload(description, allowComments, this.file)
-      .subscribe(() => {
-        this.alertService.success('Upload complete', true);
+      .pipe(finalize(() => {
         this.router.navigate(['/user', this.userService.getUserName()]);
-      });
+      }))
+      .subscribe(
+        (event: HttpEvent<any>) => {
+          if(event.type == HttpEventType.UploadProgress){
+            this.percentDone = Math.round(100 * event.loaded / event.total);
+          } else if(event instanceof HttpResponse) {
+            this.alertService.success('Upload complete', true);
+          }
+        },
+        err => {
+          console.log(err);
+          this.alertService.danger('Upload error', true);
+        }
+      );
 
     // console.log(description);
     // console.log(allowComments);
