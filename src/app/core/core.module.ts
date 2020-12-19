215401db2af069e36c280fc031b12cd4bb2f816e
William <william.lopes@pravaler.com.br>
2020-12-19 18:57:13 -0300

    Implementando o Menu com opçao Upload

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,6 +7,8 @@ import { RequestInterceptor } from './auth/request.interceptor';
 import { FooterComponent } from './footer/footer.component';
 import { AlertModule } from '../shared/components/alert/alert.module';
 import { LoadingModule } from '../shared/components/loading/loading.module';
+import { MenuModule } from '../shared/components/menu/menu.module';
+import { ShowIfLoggedModule } from '../shared/directives/show-if-logged/show-if-logged.module';
 
 @NgModule({
     declarations: [
@@ -21,7 +23,9 @@ import { LoadingModule } from '../shared/components/loading/loading.module';
         CommonModule,
         RouterModule,
         AlertModule,
-        LoadingModule
+        LoadingModule,
+        MenuModule,
+        ShowIfLoggedModule
     ],
     providers: [
         {
