c6225c58fcc804d1beb1708aea41ab4261e05afe
William <william.lopes@pravaler.com.br>
2020-12-21 23:50:07 -0300

    Implementando o redirecionamento de erro

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,6 +1,8 @@
 import { LocationStrategy, PathLocationStrategy } from "@angular/common";
 import { ErrorHandler, Injectable, Injector } from "@angular/core";
+import { Router } from "@angular/router";
 import { UserService } from "src/app/core/user/user.service";
+import { environment } from "src/environments/environment";
 import * as StackTrace from "stacktrace-js";
 import { ServerLogService } from "./server-log.service";
 
@@ -17,6 +19,7 @@ export class GlobalErrorHandler implements ErrorHandler {
         const location = this.injector.get(LocationStrategy);
         const userService = this.injector.get(UserService);
         const serverLogService = this.injector.get(ServerLogService);
+        const router = this.injector.get(Router);
 
         const url = location instanceof PathLocationStrategy
             ? location.path()
@@ -25,6 +28,8 @@ export class GlobalErrorHandler implements ErrorHandler {
         const message = error.message
             ? error.message :
             error.toString();
+
+        if(environment.production) router.navigate(['/error']);
 
         StackTrace
             .fromError(error)
